4a07b3f51402167d191ad7b1f1ed82f9eb775698
marjanhasan <moonmarjanhasan@gmail.com>
2024-09-04 20:45:30 +0600

    configuration done

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ const app = express();
 
 // jkhn e middleware use krar drkr hbe tkhn e app.use() lagbe
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+// array of accecpted urls. * for all acception
 app.use(express.json({ limit: "16kb" }));
+// json file limited asbe
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// url thk data asle setake encoded krte hoy. extended means object er vitore object ashe
 app.use(express.static("public"));
-app.use(express.cookieParser());
+// jei file e sob upload hbe
+app.use(cookieParser());
+// cookies crud krar jnno
 export { app };
